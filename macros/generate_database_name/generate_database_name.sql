e7272361400dac82691bf40a75a1920763c38788
Chaim Turkel <cyturel@gmail.com>
2025-03-25 14:59:46 +0200

    feat(infra): example data mapper

--- macros/generate_database_name/generate_database_name.sql
+++ macros/generate_database_name/generate_database_name.sql
@@ -1,25 +1,42 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
-    {%- set marts_extension = var('marts_extension','_marts') -%}    
-    {%- set staging_extension = var('staging_extension','_stg') -%}    
-
+    
+    {%- set enable_separate_db = var('enable_separate_db',False) -%}    
     {%- set default_database = target.database -%}    
-    {%- if node  and node["package_name"] == "tikal_dbt" -%}
-        {%- if "gold" in node["fqn"] -%}
-            {% set modified_database = default_database ~ marts_extension %}            
-        {%- elif "silver" in node["fqn"] -%}
-            {% set modified_database = default_database ~ staging_extension %}
-        {% else %}
-            {% set modified_database = default_database %}
-        {%- endif -%}                
+    {%- if enable_separate_db -%}
+        {%- set marts_extension = var('marts_extension','_marts') -%}    
+        {%- set staging_extension = var('staging_extension','_stg') -%}    
+        {%- set package_name = var('tikal_dbt','tikal_dbt') -%}    
+        {%- set marts_fqn = var('marts_fqn','marts') -%}    
+        {%- set staging_fqn = var('staging_fqn','staging') -%}        
 
-        {{ modified_database  }}
+        {%- if node  and node["package_name"] == package_name -%}
+            {%- if marts_fqn in node["fqn"] -%}
+                {% set modified_database = default_database ~ marts_extension %}            
+            {%- elif staging_fqn in node["fqn"] -%}
+                {% set modified_database = default_database ~ staging_extension %}
+            {% else %}
+                {% set modified_database = default_database %}
+            {%- endif -%}                
+
+            {{ modified_database  }}
+        {% else %}
+            {%- if custom_database_name is none -%}
+                {{ default_database }}
+            {%- else -%}
+                {{ custom_database_name | trim }}
+            {%- endif -%}
+
+        {%- endif -%}
     {% else %}
         {%- if custom_database_name is none -%}
-            {{ default_database }}
-        {%- else -%}
-            {{ custom_database_name | trim }}
-        {%- endif -%}
 
-    {%- endif -%}
-    
+            {{ default_database }}
+
+        {%- else -%}
+
+            {{ custom_database_name | trim }}
+
+        {%- endif -%}
+        
+    {%- endif -%}    
 {%- endmacro %}
--- macros/generate_schema_name/generate_schema_name.sql
+++ macros/generate_schema_name/generate_schema_name.sql
@@ -1,10 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set staging_fqn = var('staging_fqn','_stg') -%}    
+    {%- set package_name = var('package_name','tikal_dbt') -%}    
     {%- if node["resource_type"]=="model" -%}
         {% set dir_path = node["path"].split('/') %}
         {% set model_type = dir_path[1] %}
         
-        {%- if node["package_name"]=="tikal_dbt" -%}
+        {%- if node["package_name"]==package_name -%}
             {% set domain = node["fqn"][1] %}
             {%- if staging_fqn in node["fqn"] -%}
                 {% set position = node["fqn"].index(staging_fqn)-1 %}
